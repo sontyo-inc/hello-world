a7c6fbabeb016c6c7a5634a942bb2b80580ec76a
souichirou <souichirou112358@gmail.com>
2017-11-11 14:58:35 +0900

    タイムゾーンを東京に設定

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,7 @@ Bundler.require(*Rails.groups)
 
 module HelloWorld
   class Application < Rails::Application
+    config.time_zone = 'Tokyo'
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
