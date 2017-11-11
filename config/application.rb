81da7d47741cb60474903a7d9acc043187d09cb0
souichirou <souichirou112358@gmail.com>
2017-11-11 14:59:20 +0900

    rails gの際に余計なファイルを生成しないように設定

--- config/application.rb
+++ config/application.rb
@@ -8,5 +8,11 @@ module HelloWorld
   class Application < Rails::Application
     config.time_zone = 'Tokyo'
     config.active_record.raise_in_transactional_callbacks = true
+    config.generators do |g|
+      g.helper false
+      g.stylesheets false
+      g.javascripts false
+      g.test_framework false
+    end
   end
 end
