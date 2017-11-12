ee754408fbb691eba916a70a21fc8d8baafdc0f3
souichirou <souichirou112358@gmail.com>
2017-11-12 11:10:17 +0900

    ログイン、ログアウトの時のルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root :to => 'users#index'
   resources :users
   resources :user_sessions, only: [:new, :create, :destroy]
+
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'logout' => 'user_sessions#destroy', :as => :logout
 end
